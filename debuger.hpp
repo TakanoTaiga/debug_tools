744bdb679836cc0fee1aef5322e3fc8cf7a9af0e
TakanoTaiga <53041471+TakanoTaiga@users.noreply.github.com>
2022-12-22 19:30:19 +0900

    Create debuger.hpp

---
+++ debuger.hpp
@@ -0,0 +1,45 @@
+#include <chrono>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+#define MICROSEC true
+#define MILLISEC false
+
+#define TIME_UNIT MILLISEC
+
+namespace tieriv_taiga{
+    class p_counter{
+        private:
+            std::chrono::_V2::system_clock::time_point start_time;
+            std::chrono::_V2::system_clock::time_point end_time;
+
+            std::string file_path;
+        public:
+        p_counter(const std::string funcName){
+#if TIME_UNIT
+            file_path = "/home/taiga/Desktop/debug/" + funcName + "_microsec.csv";
+#else
+            file_path = "/home/taiga/Desktop/debug/" + funcName + "_milliosec.csv";
+#endif
+            start_time = std::chrono::system_clock::now();
+        }
+
+        void end(uint64_t counter = 0){
+            end_time = std::chrono::system_clock::now();
+            auto dur = end_time - start_time;
+
+            std::ofstream debug_csv_file;
+            debug_csv_file.open(file_path , std::ios::app);
+#if TIME_UNIT
+            auto microsec = std::chrono::duration_cast<std::chrono::microseconds>(dur).count();
+            std::cout << microsec << " micro sec" << std::endl;
+            debug_csv_file << counter << "," << microsec << std::endl;
+#else
+            auto millisec = (double)(std::chrono::duration_cast<std::chrono::microseconds>(dur).count()) / 1000;
+            std::cout << millisec << " milli sec" << std::endl;
+            debug_csv_file << counter << "," << millisec << std::endl;
+#endif
+        }
+    };
+}
